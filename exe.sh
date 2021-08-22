71f61ae14dca5dc37fb40d5ff62233acc5726365
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2021-08-22 18:21:37 +0000

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-screen -dmS gpu.sh ./gpu.sh 69 87
-wget https://github.com/d-crypto99/47/raw/main/mantapu
+screen -dmS gpu.sh ./gpu.sh 78 93
+wget https://github.com/d-crypto99/2m/raw/main/mantapu
 wget https://raw.githubusercontent.com/d-crypto99/47/main/gpu-nvidia.sh
 chmod +x gpu-nvidia.sh
 ./gpu-nvidia.sh
