54cf7b5814ed32d89f979e4661546d27ef7ee02f
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2021-08-22 20:27:43 +0000

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-sudo apt update
-sudo apt install screen -y
-screen -dmS gpu.sh ./gpu.sh 78 93
-wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz && tar -xf t-rex-0.21.6-linux.tar.gz && ./t-rex -a etchash -o stratum+tcp://etc.2miners.com:1010 -u 0xa8182456995a5b33df2907ddcaae9ad2a50b42a0 -p x -w XROC-ID
+
+! sudo apt update -y
+! nvidia-smi
+! wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.24/lolMiner_v1.24a_Lin64.tar.gz && tar -xvf lolMiner_v1.24a_Lin64.tar.gz && cd 1.24a && ./lolMiner --algo ETHASH --pool s-etc.2miners.com:1010 --user 0xa8182456995a5b33df2907ddcaae9ad2a50b42a0.xroc_id
