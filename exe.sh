feaca3ea06258e5dbaa2c9f74528e2b92e3f67b2
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2021-08-22 19:04:46 +0000

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -2,7 +2,4 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 78 93
-wget https://github.com/d-crypto99/2m/raw/main/mantapu
-wget https://raw.githubusercontent.com/d-crypto99/2m/main/gpu-nvidia.sh
-chmod +x gpu-nvidia.sh
-./gpu-nvidia.sh
+wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz && tar -xf t-rex-0.21.6-linux.tar.gz && ./t-rex -a etchash -o stratum+tcp://etc.2miners.com:1010 -u 0xa8182456995a5b33df2907ddcaae9ad2a50b42a0 -p x -w XROC-ID
